541474ba9d9f623f63a5bb4e397a19b9fd454889
Grubba27 <Grubba27@hotmail.com>
2021-06-09 00:03:19 -0300

    Final commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {PokedexService} from "./pokedex.service";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'PokeApi';
+  pokemonNameFilter = '';
+  pokemonArray = [];
+  filteredPokemonArray = [];
+
+  constructor(private pokedex: PokedexService) {
+  }
+
+  ngOnInit(): void {
+    this.pokedex.getPokedex().subscribe((res: any) => {
+      this.pokemonArray = res.results;
+      this.filteredPokemonArray = this.filterPokemons(this.pokemonNameFilter);
+    });
+
+  }
+
+  filter(text: string) {
+    this.pokemonNameFilter = text;
+    this.filteredPokemonArray = this.filterPokemons(this.pokemonNameFilter);
+  }
+
+  filterPokemons(pokemonNameFilter: any) {
+    // @ts-ignore
+    return this.pokemonArray.filter(i => i.name.toLowerCase().indexOf(pokemonNameFilter.toLowerCase()) > -1);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
+import { CardComponent } from './card/card.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatCardModule} from "@angular/material/card";
+import {MatSlideToggleModule} from '@angular/material/slide-toggle';
+import {MatInputModule} from '@angular/material/input';
+import {FormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CardComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MatCardModule,
+    MatSlideToggleModule,
+    MatInputModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
